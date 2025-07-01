b59b9ee87a1934567027b941d60088d49cbfc692
18aman2 <amanshaikh2020as@gmail.com>
2025-07-01 15:20:41 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () =>{
           show={showPopup}
           onClose={handleClosePopup}
           studentName="Namrata patil"
-          studentPhotoUrl="np.jpeg"// Path to their photo
+          studentPhotoUrl="Images/np.jpeg"   // Path to their photo
           uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
         />
       </div>
